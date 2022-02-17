8180a943a0f99de14518a4e250e01f2fa8505f23
Diones Pereira Alves <pereiradiones987@gmail.com>
2022-02-17 14:25:08 -0300

    atualizando a implementação

--- src/app.js
+++ src/app.js
@@ -11,45 +11,71 @@ const User = require('./models/User');
 app.use(express.json());
 
 app.get('/', eAdmin , async (req, res) => {
-  return res.json({
-    erro: false,
-    mensagem: "Listar usuários",
-    id_usuario_logado: req.userId
+  await User.findAll({
+    attributes: ['id', 'name', 'email'],
+    order: [['id', "DESC"]]
+
+  }).then((users) => {
+    return res.json({
+      erro: false,
+      users: users,
+      id_usuario_logado: req.userId
+    });
+
+  }).catch(() => {
+    return res.status(400).json({
+      erro: true,
+      mensagem: "ERRO: nenhum usuário encontrado!"
+    });
   });
+
 
 });
 
 app.post('/cadastrar', async (req, res) => {
   //$2a$08$wlYEWbiJA.j./LBPUFpitee3t2sdUKPmAzmim.OoPxKku9dB7WfRO
-  const password =  await bcrypt.hash("123456", 8);
+  var dados = req.body;
+  dados.password =  await bcrypt.hash(dados.password, 8);
 
-  console.log(password);
-  return res.json({
-    erro: false,
-    mensagem: "Usuario cadastrado com sucesso!"
+  //console.log(dados);
+
+  await User.create(dados).then(() => {    
+    return res.json({
+      erro: false,
+      mensagem: "Usuario cadastrado com sucesso!"
+    });
+  }).catch(() => {
+    return res.status(400).json({
+      erro: true,
+      mensagem: "ERRO: Não foi possível cadastrar o usuário!"
+    });
   });
-
+    
 });
 
 app.post('/login', async (req, res) => {
+  const user = await User.findOne({
+    attributes: ['id', 'name', 'email', 'password'],
+    where: {
+      email: req.body.email
+    }
+  });
 
-  console.log(req.body);
-
-  if(req.body.email != "pereiradiones987@gmail.com") {
+  if(user === null) {
     return res.status(400).json({
       erro: true,
-      mensagem: "Erro: usuário ou senha incorreta! E-mail incorreto! "
+      mensagem: "ERRO: usuário ou senha incorreta!"
     });
   }
 
-  if(!(await bcrypt.compare(req.body.password, "$2a$08$wlYEWbiJA.j./LBPUFpitee3t2sdUKPmAzmim.OoPxKku9dB7WfRO") )) {
+  if(!(await bcrypt.compare(req.body.password, user.password) )) {
     return res.status(400).json({
       erro: true,
-      mensagem: "Erro: usuário ou senha incorreta! Senha incorreto! "
+      mensagem: "Erro: usuário ou senha incorreta!"
     });
   }
 
-  let token = jwt.sign({ id: 3 }, "SW2C56NHU7891DXCX2@M#OI+-JY65AW", {
+  let token = jwt.sign({ id: user.id }, "SW2C56NHU7891DXCX2@M#OI+-JY65AW", {
     //expiresIn: 600 // 10 MINUTOS
     //expiresIn: 60 // 1 min    
     expiresIn: '7D' // 7 dias    
@@ -63,7 +89,10 @@ app.post('/login', async (req, res) => {
 
 });
 
-conn.sync().then(() => {
+conn
+//.sync({ force: true })
+.sync()
+.then(() => {
 
   app.listen(5000, () => {
     console.log(chalk.bgGreen.blackBright('Servidor rodando na porta 5000'));
